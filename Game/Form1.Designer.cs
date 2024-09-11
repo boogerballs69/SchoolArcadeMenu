7a262445cf40699f3e9fb4263c0f3e88568f5b5a
zackwhit694 <zackwhit694@SDS-L-763HC63>
2024-09-11 11:09:18 -0400

    Add project files.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Game
+{
+    partial class arcadeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(arcadeForm));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(330, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(135, 33);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "ARCADE";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.Control;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(271, 120);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(248, 171);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // arcadeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "arcadeForm";
+            this.Text = "Arcade";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Game
+{
+    public partial class arcadeForm : Form
+    {
+        public arcadeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.beep);
+            startButton.Text = "3";
+            Refresh();
+            sp.Play();
+            Thread.Sleep(1000);
+            startButton.Text = "2";
+            Refresh();
+            sp.Play();
+            Thread.Sleep(1000);
+            startButton.Text = "1";
+            Refresh();
+            sp.Play();
+            Thread.Sleep(1000);
+            startButton.Text = "Go!";
+            Refresh();
+            sp.Play();
+            Thread.Sleep(1000);
+            startButton.Visible = false;
+        }
+    }
+}
